cb1e675c9cd8a6df8955c81a3dedfb02b7cc4a31
robinbuschmann <robinbuschmann@hotmail.de>
2019-08-02 20:05:06 +0200

    Updates dependencies

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,9 +1,7 @@
 import {Sequelize} from 'sequelize-typescript';
-import {Op} from 'sequelize';
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  operatorsAliases: Op,
   database: 'movies',
   storage: ':memory:',
   models: [__dirname + '/models']
